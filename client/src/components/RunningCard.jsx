0a8188e68406dc6eaac1def564ecb0f3dfd362ed
Olivia Schwartz <ocmooney@gmail.com>
2022-10-27 23:40:15 -0400

    Styling of Run Card

--- client/src/components/RunningCard.jsx
+++ client/src/components/RunningCard.jsx
@@ -7,10 +7,10 @@ const RunningCard= ({onClick, date, distance, time, difficulty, run}) => {
     return (
         <div className="card run-card"onClick={() => onClick(run)}  >
             <div className="info-wrapper flex-col">
-                <h3>Run Date: {date}</h3>
-                <p>Distance:{distance}</p>
-                <p>Time (minutes): {time} </p>
-                <p>Run difficulty: {difficulty}</p>
+                <h3 className="runDateDisplay">Run Date: {date}</h3>
+                <p className="cardDisplay">Distance: {distance}</p>
+                <p className="cardDisplay">Time (minutes): {time} </p>
+                <p className="cardDisplay">Run difficulty: {difficulty}</p>
             </div>
 
         </div>
